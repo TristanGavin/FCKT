01deeb3c36e283df275241d820337df8dd2e28ad
TristanGavin <gavin.tristan@gmail.com>
2020-02-24 13:35:05 -0800

    added the abstract syntax for expr test stmt var

--- Project.hs
+++ Project.hs
@@ -1,3 +1,24 @@
+-- *Abstract Syntax
+
+--  
+--  expr := 'Get'                                  gets the variable value
+--        | int                                    int literal
+--        | expr '+' expr                          add two expressions
+--        | expr '*' expr                          multiply two expression
+--        | expr '/' expr                          divide two expressions.
+--        | If expr 'then' expr 'else' expr        if else statmment 
+--
+--  var := string type                             defining a variable
+--  
+--  test := expr '=' expr                          check if two expr are equal                 
+--        | expr '<' expr                          less than
+--        | expr '>'                               greater than
+--        | expr '<=' expr                         Less than or equal to
+--        | expr '>=' expr                         Greater or equal to. 
+--
+--  data stmt := Set                               assigns value to a var
+--
+
 
 data Expr
    = Get Var
