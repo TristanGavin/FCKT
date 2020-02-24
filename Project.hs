d8a12d62938cea2705e9ee4d3f3229c6665d09f2
hernafra <56043516+hernafra@users.noreply.github.com>
2020-02-24 13:08:38 -0800

    Update Project.hs

--- Project.hs
+++ Project.hs
@@ -1,29 +1,25 @@
 
-data Expr
-   = Get Var
-   | Lit Int
-   | Add Expr Expr
-   | Mul Expr Expr
---   | Div Expr Expr
-   | If Expr Expr Expr
-  deriving (Eq,Show)
+data Expr = Get Var
+          | Lit Int
+          | Add Expr Expr
+          | Mul Expr Expr
+--        | Div Expr Expr
+          | If Expr Expr Expr
+         deriving (Eq,Show)
 
--- type String = [Char]
+type String = [Char]
 
-data Var
-   = String 
-   | String Type Expr
-  deriving (Eq, Show)
+data Var  = String 
+          | String Type Expr
+         deriving (Eq, Show)
   
-data Test
-   = Equ Expr Expr
-   | LTE Expr Expr
-   | GTE Expr Expr
-  deriving (Eq,Show)
+data Test = Equ Expr Expr
+          | LTE Expr Expr
+          | GTE Expr Expr
+         deriving (Eq,Show)
 
-data Stmt
-   = Set Var Expr
+data Stmt = Set Var Expr
    -- | While Test Stmt
    -- | Begin [Stmt]
-  deriving (Eq,Show)
+         deriving (Eq,Show)
 
