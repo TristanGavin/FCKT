98abef805c8428c9193c3ecb24c2b7f66d845e13
SmallAndLarge <59236839+SmallAndLarge@users.noreply.github.com>
2020-02-26 12:38:07 -0800

    Up to program

--- Project.hs
+++ Project.hs
@@ -1,12 +1,17 @@
 -- *Abstract Syntax
+module Fckt where
 
---  
+import Data.Map (Map,fromList,lookup,insert)
+import Data.Maybe (fromJust)
+import Prelude hiding (lookup)
+
+--
 --  expr := 'Get'                                  gets the variable value
 --        | int                                    int literal
 --        | expr '+' expr                          add two expressions
 --        | expr '*' expr                          multiply two expression
 --        | expr '/' expr                          divide two expressions.
---        | If expr 'then' expr 'else' expr        if else statmment 
+--        | If expr 'then' expr 'else' expr        if else statmment
 --
 --  type := int                                    integer value
 --        | bool                                   boolean value
@@ -15,47 +20,68 @@
 --        | string                                 array of chars
 --
 --  var := string type                             defining a variable
---  
---  test := expr '=' expr                          check if two expr are equal                 
+--
+--  test := expr '=' expr                          check if two expr are equal
 --        | expr '<' expr                          less than
 --        | expr '>'                               greater than
 --        | expr '<=' expr                         Less than or equal to
---        | expr '>=' expr                         Greater or equal to. 
+--        | expr '>=' expr                         Greater or equal to.
 --
 --  data stmt := Set                               assigns value to a var
 --
+type Var = String
+-- Data type that
+-- data TypeVal = IntI    Int
+--              | BoolB   Bool
+--              | FloatF  Float
+--              | CharC   Char
+--              | StringS String
+--              | TypeError
+type Number = Either Int Float
 
--- Data type that 
-data TypeVal = IntI    Int
-             | BoolB   Bool
-             | FloatF  Float
-             | CharC   Char
-             | StringS String
-             | TypeError
-          
+data Type = TypeInt
+          | TypeBool
+          | TypeFloat
+          -- | TypeChar
+          | TypeString
+          | TypeError
+
 data Expr    = Get Var
-             | TypeVal Val
+             | Lit Number
+             | Str String
              | Add Expr Expr
              | Mul Expr Expr
              | Div Expr Expr
-             | If Expr Expr Expr
+             | Equ Expr Expr
+             | LT  Expr Expr
+             | Not Expr
+             -- | If Expr Expr Expr
             deriving (Eq,Show)
 
--- type String = [Char]
+data Line    = Set Var Expr
+             | If Expr Line Line
+             | While Expr Line
+             | Chunk [Line]
+             deriving (Eq, Show)
 
--- Defining a variable ie. from haskell to c code
--- Var x TypeInt Lit 5 => int x = 5;
-data Var     = Var [Char] Type Expr 
--- Sugar         | String Type Expr
-         deriving (Eq, Show)
-  
-data Test = Equ Expr Expr
-          | LTE Expr Expr
--- Sugar          | GTE Expr Expr
-         deriving (Eq,Show)
+type Decl    = (Var, Type)
 
-data Stmt = Set Var Expr
-   -- | While Test Stmt
-   -- | Begin [Stmt]
-         deriving (Eq,Show)
+data Program = Prog [Decl] Line
+             deriving (Eq, Show)
 
+--
+-- -- Defining a variable ie. from haskell to c code
+-- -- Var x TypeInt Lit 5 => int x = 5;
+-- data Var     = Var [Char] Type Expr
+-- -- Sugar         | String Type Expr
+--          deriving (Eq, Show)
+--
+-- data Test = Equ Expr Expr
+--           | LTE Expr Expr
+-- -- Sugar          | GTE Expr Expr
+--          deriving (Eq,Show)
+--
+-- data Stmt = Set Var Expr
+--    -- | While Test Stmt
+--    -- | Begin [Stmt]
+--          deriving (Eq,Show)
